01db9bdc097a90e8ed52c1390a4692f490076155
lethan3 <ethanlnow@gmail.com>
2024-08-14 23:17:58 -0700

    blackhole triangle

--- blackhole_main.cpp
+++ blackhole_main.cpp
@@ -2,13 +2,39 @@
 
 typedef State<Value, HValue, Move> StateT;
 int main() {
-    n = 5;
+    n = 7;
     board_adj.resize(2 * n + 1);
 
-    for (int i = 0; i < 2 * n; ++i) {
-        board_adj[i].push_back(i + 1);
-        board_adj[i + 1].push_back(i);
+    int k = 5;
+    std::vector<std::vector<int>> tri(k);
+    int curr = 0;
+    for (int i = 0; i < k; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            tri[i].push_back(curr);
+            curr++;
+        }
     }
+
+    for (int i = 0; i < k; ++i) {
+        for (int j = 0; j < (int)tri[i].size() - 1; ++j) {
+            board_adj[tri[i][j]].push_back(tri[i][j + 1]);
+            board_adj[tri[i][j + 1]].push_back(tri[i][j]);
+        }
+    }
+
+    for (int i = 0; i < k - 1; ++i) {
+        for (int j = 0; j < (int)tri[i].size(); ++j) {
+            board_adj[tri[i][j]].push_back(tri[i + 1][j]);
+            board_adj[tri[i + 1][j]].push_back(tri[i][j]);
+            board_adj[tri[i][j]].push_back(tri[i + 1][j + 1]);
+            board_adj[tri[i + 1][j + 1]].push_back(tri[i][j]);
+        }
+    }
+
+    // for (int i = 0; i < 2 * n; ++i) {
+    //     board_adj[i].push_back(i + 1);
+    //     board_adj[i + 1].push_back(i);
+    // }
 
     Minimax<State, Value, HValue, Move> minimax;
     StateT initstate;
